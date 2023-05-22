cc12d333f848ef8e0bd25784d108412fcbf89566
An Oleg <111381525+mranolegprivate@users.noreply.github.com>
2023-05-22 14:04:49 +0300

    Update HW_1.sql

--- HW_1.sql
+++ HW_1.sql
@@ -51,74 +51,113 @@ CREATE TABLE public.students (
  WHERE created_on  = '2021-03-26 00:00:00'
  
  10. Вывести пользователей где в имени есть слово Анна
- select * 
- from students
- where name like '%Анна%'
+ SELECT * 
+ FROM students
+ WHERE name LIKE '%Анна%'
  
  11. Вывести пользователей где в имени в конце есть 8
- select * 
- from students
- where name like '%8'
+ SELECT * 
+ FROM students
+ WHERE name LIKE '%8'
  
  12. Вывести пользователей где в имени в есть буква а
- 
+ SELECT * 
+ FROM students
+ WHERE name LIKE '%a%'
  
  13. Вывести пользователей которые были созданы 2021-07-12 00:00:00
- 
+ SELECT * 
+ FROM students
+ WHERE created_on = '2021-07-12 00:00:00'
  
  14. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
- 
+ SELECT * 
+ FROM students
+ WHERE created_on = '2021-07-12 00:00:00' and password = '1m313'
  
  15. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
- 
+ SELECT * 
+ FROM students
+ WHERE created_on = '2021-07-12 00:00:00' and name LIKE '%Andrey%'
  
  16. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
- 
+ SELECT * 
+ FROM students
+ WHERE created_on = '2021-07-12 00:00:00' and name LIKE '%8%'
  
  17. Вывести пользователя у которых id равен 110
- 
+ SELECT * 
+ FROM students
+ WHERE id = '110'
  
  18. Вывести пользователя у которых id равен 153
- 
+ SELECT * 
+ FROM students
+ WHERE id = '153'
  
  19. Вывести пользователя у которых id больше 140
- 
+ SELECT * 
+ FROM students
+ WHERE id > '140'
  
  20. Вывести пользователя у которых id меньше 130
- 
+ SELECT * 
+ FROM students
+ WHERE id < '130'
  
  21. Вывести пользователя у которых id меньше 127 или больше 188
- 
+ SELECT * 
+ FROM students
+ WHERE id < '127' or id > '188'
  
  22. Вывести пользователя у которых id меньше либо равно 137
- 
+ SELECT * 
+ FROM students
+ WHERE id <= '137'
  
  23. Вывести пользователя у которых id больше либо равно 137
- 
+ SELECT * 
+ FROM students
+ WHERE id >= '137'
  
  24. Вывести пользователя у которых id больше 180 но меньше 190
- 
+ SELECT * 
+ FROM students
+ WHERE id > '180' AND id < '190'
  
  25. Вывести пользователя у которых id между 180 и 190
- 
+ SELECT * 
+ FROM students
+ WHERE id between '180' AND '190'
  
  26. Вывести пользователей где password равен 12333, 1m313, 123313
- 
+ SELECT * 
+ FROM students
+ WHERE password in('12333', '1m313', '123313')
  
  27. Вывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
- 
+ SELECT * 
+ FROM students
+ WHERE created_on in ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00')
  
  28. Вывести минимальный id.
- 
+ SELECT MIN(id) 
+ FROM students
  
  29. Вывести максимальный.
- 
+ SELECT MAX(id) 
+ FROM students
  
  30. Вывести количество пользователей.
- 
+ SELECT COUNT(id) 
+ FROM students
  
  31. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку возрастания даты добавления пользоватлеля.
- 
+ SELECT id, name, created_on
+ FROM students
+ ORDER BY created_on ASC
  
  32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
- 
+ SELECT id, name, created_on
+ FROM students
+ ORDER BY created_on DESC
