48b2968f830d70c6a0cdd6fe901074a42283e161
An Oleg <111381525+mranolegprivate@users.noreply.github.com>
2023-05-23 16:33:58 +0300

    Update HW_2.sql

--- HW_2.sql
+++ HW_2.sql
@@ -13,9 +13,9 @@ Pass: 123
  1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами.
 	select employee_name, monthly_salary
 	from (
-	(employee_salary e_s inner join employees e
-	on e_s.employee_id = e.id) inner join salary s 
-	on e_s.salary_id = s.id)
+	(employee_salary es inner join employees e
+	on es.employee_id = e.id) inner join salary s 
+	on es.salary_id = s.id)
  
  2. Вывести всех работников у которых ЗП меньше 2000.
  
